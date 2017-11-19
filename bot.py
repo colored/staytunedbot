e2e6840b8b1fc7aa403cac438d99ee0042daaeb3
oleksandrzubko <300000001mps@gmail.com>
2017-11-18 18:33:50 -0600

    mock basic test mock Bot class

---
+++ bot.py
@@ -0,0 +1,19 @@
+class Bot():
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    #add_subscription
+    def follow(self, id):
+        pass
+
+    #remove subsription
+    def unfollow(self, id):
+        pass
+
+    #gets list of follows
+    def get_follows(self):
+        return []
+
+
+
---
+++ discogs_connector.py
@@ -0,0 +1,8 @@
+def find_id(artist):
+    pass
+
+def find_id(label):
+    pass
+
+def get_all_releases(id):
+    pass
